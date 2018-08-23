4a45cb68dcff51d6b053cea20f63c2c9e385cc70
Sarah Pai <pai.sarah89@gmail.com>
2018-08-23 00:43:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     return 'I can\'t hear you!'
   } else if( string === string.toUpperCase()){
     return 'YES INDEED!'
-  } else if (string === string.toLowerCase()); {
+  } else if (string === "I love you, Grandma.") {
     return 'I can\'t hear you!'
   }
 }
